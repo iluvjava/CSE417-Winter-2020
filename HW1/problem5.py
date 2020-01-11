dc7494bc3b209710c04d57a5eb944896f548cf26
A.L <32211153+iluvjava@users.noreply.github.com>
2020-01-11 12:46:34 -0800

    stuff

---
+++ HW1/problem5.py
@@ -0,0 +1,13 @@
+"""
+Name: Hongda Li
+Class cse 417
+This file is for hw1, problem 5.
+
+m.rank() -> The choice of m after the perfect matching algorithms.
+M.goodness -> sum_{i=0}^{n-1}m.rank()/n
+"""
+
+def rand_permutation():
+
+    pass
+
